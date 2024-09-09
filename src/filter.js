a091a6f9d5eeb44df3397c0dfa2085b40b851c65
Tsilavina <159104790+TsilavinaZh@users.noreply.github.com>
2024-09-09 15:23:59 +0300

    Napina regex

--- src/filter.js
+++ src/filter.js
@@ -3,30 +3,35 @@ const words = require('./words.json').words;
 class Filter {
   constructor() {
     this.words = new Set(words);
+    this.regex = this.createRegex();
+  }
+
+  createRegex() {
+    const wordList = Array.from(this.words).map(word => `\\b${word}\\b`).join('|');
+    return new RegExp(wordList, 'gi');
   }
 
   isProfane(text) {
-    const lowerCaseText = text.toLowerCase();
-    return Array.from(this.words).some(word => lowerCaseText.includes(word));
+    return this.regex.test(text);
   }
 
   censor(text) {
-    const lowerCaseText = text.toLowerCase();
-    return lowerCaseText.split(' ').map(word => {
-      return this.words.has(word) ? '*'.repeat(word.length) : word;
-    }).join(' ');
+    return text.replace(this.regex, match => '*'.repeat(match.length));
   }
 
   addWords(...newWords) {
     newWords.forEach(word => this.words.add(word.toLowerCase()));
+    this.regex = this.createRegex();
   }
 
   removeWords(...removeWords) {
     removeWords.forEach(word => this.words.delete(word.toLowerCase()));
+    this.regex = this.createRegex();
   }
 
   clearList() {
     this.words.clear();
+    this.regex = this.createRegex();
   }
 }
 
